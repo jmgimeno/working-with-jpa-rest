a7f56227747ba17ae0e1d52e700d5ad0edd7c71e
Juan Manuel Gimeno Illa <jmgimeno@gmail.com>
2018-09-28 07:30:26 +0200

    Initializr project.

---
+++ src/main/java/cat/udl/eps/engsoftarch/workingwithjparest/WorkingWithJpaRestApplication.java
@@ -0,0 +1,12 @@
+package cat.udl.eps.engsoftarch.workingwithjparest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkingWithJpaRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkingWithJpaRestApplication.class, args);
+    }
+}
